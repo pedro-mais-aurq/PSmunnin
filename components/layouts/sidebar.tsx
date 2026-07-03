014277bca45c9fd619bbef78babfdd7c943f710c
pedro-mais-aurq <pedrolu690@gmail.com>
2026-07-03 15:56:00 -0300

    Add files via upload

---
+++ components/layouts/sidebar.tsx
@@ -0,0 +1,7 @@
+export function Sidebar() {
+    return (
+        <div className="w-64 min-h-screen border-r flex justify-start">
+            <h1 className="text-left">Sidebar</h1>
+        </div>
+    )
+}
